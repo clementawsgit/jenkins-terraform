bbe5658b4f615a0e7de4be15d2e34334e1c03bb3
clement <jeromeit05@gmail.com>
2025-03-08 17:18:49 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,18 +3,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-04b4f1a9cf54c11d0"
+  ami           = "ami-04b4f1a9cf54c11d0" # Replace with your AMI
   instance_type = "t2.micro"
-
+  count = "1"
   tags = {
-    Name = "Jenkins-Deployed-EC2"
+    Name = "MyInstance"
   }
-
-  key_name = "ubuntu"
-
-  vpc_security_group_ids = ["sg-062d44a4c696a86f2"]
-}
-
-output "public_ip" {
-  value = aws_instance.example.public_ip
 }
