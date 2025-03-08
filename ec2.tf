1c49dc5e8f6a54a165bb41b20869a252f0519a65
clement <jeromeit05@gmail.com>
2025-03-08 15:25:36 +0800

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,21 +1,20 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 provider "aws" {
-    region = "us-east-1"  
+  region = "us-east-1"
 }
 
-resource "aws_instance" "foo" {
+resource "aws_instance" "example" {
   ami           = "ami-04b4f1a9cf54c11d0"
-  key_name      = "ubuntu"
   instance_type = "t2.micro"
+
   tags = {
-      Name = "TF-Instance"
+    Name = "Jenkins-Deployed-EC2"
   }
+
+  key_name = "ubuntu"
+
+  vpc_security_group_ids = ["sg-062d44a4c696a86f2"]
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
 }
