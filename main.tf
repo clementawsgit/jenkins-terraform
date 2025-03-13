927325dcb0d579f31871abf074adf0dfc818f41b
clement <jeromeit05@gmail.com>
2025-03-13 17:58:24 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-04b4f1a9cf54c11d0" # us-west-2
+  ami           = "ami-04b4f1a9cf54c11d0"
   instance_type = "t2.micro"
   tags = {
       Name = "TF-Instance"
